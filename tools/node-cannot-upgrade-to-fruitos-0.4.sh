093d180ca10f29055918326887c718002aa31137
Herry <herry13@gmail.com>
2018-06-22 02:56:21 +0100

    added script node-cannot-upgrade-to-fruitos-0.4.sh

---
+++ tools/node-cannot-upgrade-to-fruitos-0.4.sh
@@ -0,0 +1,12 @@
+# Below command generates a list of nodes that cannot be
+# upgraded to FruitOS 0.4 since their root partitions
+# are not large enough. They should be upgraded by flashing
+# the SSD.
+
+fruit-cli monitor \
+  | sh JSON.sh \
+  | grep '"volumes","/media/root-ro","size"' \
+  | sed 's/","volumes","\/media\/root-ro","size"\]//g' \
+  | sed 's/\["//g' \
+  | sed 's/"//g' \
+  | awk '{if ($2 < 400000) print $1 " " $2}'
