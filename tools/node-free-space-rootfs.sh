9579a0503d41108ab4e6174a1d5172cd74eeefb8
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2018-10-12 14:35:41 +0100

    Remove tools from previous releases

--- tools/node-free-space-rootfs.sh
+++
@@ -1,7 +0,0 @@
-fruit-cli monitor \
-  | sh $(dirname $0)/../JSON.sh \
-  | grep '"volumes","/media/root-ro","free"' \
-  | sed 's/","volumes","\/media\/root-ro","free"\]//g' \
-  | sed 's/\["//g' \
-  | sed 's/"//g' \
-  | awk '{print $2 " " $1}'
